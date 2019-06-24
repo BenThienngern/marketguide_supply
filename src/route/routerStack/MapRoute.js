58e9ea49069e34aafd4596fac6cea996d26ba4ce
Ben <50435591+BenThienngern@users.noreply.github.com>
2019-06-24 17:14:39 +0700

    Map

--- src/route/routerStack/MapRoute.js
+++ src/route/routerStack/MapRoute.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, ImageBackground, Dimensions, Text } from 'react-native';
+import ImageZoom from 'react-native-image-pan-zoom';
 import HeadBanner from '../../components/HeadBanner';
 
-const UserRoute = ({ navigation }) => (
-  <View>
-    <HeadBanner
-      headerText={'Map'}
-      isShowBackIcon
-      onClickBack={() => navigation.goBack()}
-    />
-  </View>
-);
+const realImg = require('../../resource/images/Real.png');
+
+class UserRoute extends Component {
+  render() {
+    return (
+      <View>
+        <View>
+          <HeadBanner
+            headerText={'Map'}
+            isShowBackIcon
+            onClickBack={() => this.props.navigation.goBack()}
+          />
+          <View>
+            <ImageZoom
+              cropWidth={Dimensions.get('window').width}
+              cropHeight={Dimensions.get('window').height}
+              imageWidth={Dimensions.get('window').width}
+              imageHeight={Dimensions.get('window').height}
+            >
+              <ImageBackground style={{ width: '100%', height: '100%' }} source={realImg}>
+                <Text style={{ marginTop: 400, marginLeft: 40 }}> h</Text>
+              </ImageBackground>
+            </ImageZoom>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default UserRoute;
